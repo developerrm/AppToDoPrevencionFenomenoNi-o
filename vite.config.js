32790e8dbf6d23b0027ceec9c2ba686b89bce7d1
LSGMAZA <162736151+LSGMAZA@users.noreply.github.com>
2026-09-23 16:52:16 -0500

    ci: deploy app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-export default defineConfig({plugins:[react(),tailwindcss()]})
+
+export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/AppToDoPrevencionFenomenoNi-o/' : '/',
+  plugins: [react(), tailwindcss()],
+})
